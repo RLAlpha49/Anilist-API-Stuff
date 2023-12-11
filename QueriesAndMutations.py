66541125c29c34b713dbdc179bc38a4d1e9a6c64
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2023-12-11 09:54:43 -0600

    Should fix message activity not getting correctly liked

--- QueriesAndMutations.py
+++ QueriesAndMutations.py
@@ -165,7 +165,7 @@ class Queries:
                         likes {
                             id
                         }
-                        messengerId
+                        recipientId
                     }
             """
         Query += """
